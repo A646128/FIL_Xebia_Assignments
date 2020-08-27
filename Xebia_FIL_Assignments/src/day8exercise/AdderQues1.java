f632147c22e1394352133f81b1323fb18516565b
Sumit <kumar07sumit@gmail.com>
2020-08-27 11:59:01 +0530

    Assignments

---
+++ Xebia_FIL_Assignments/src/day8exercise/AdderQues1.java
@@ -0,0 +1,18 @@
+package day8exercise;
+
+class Arithmetic
+{
+	static int add(int a, int b)
+	{
+	return (a+b);
+	}
+}
+
+public class AdderQues1 extends Arithmetic {
+
+	public static void main(String[] args) {
+		System.out.println("Addition of two numbers: "+AdderQues1.add(10, 10));
+		
+	}
+
+}
---
+++ Xebia_FIL_Assignments/src/day8exercise/InterfacePolymorphismQues3.java
@@ -0,0 +1,46 @@
+package day8exercise;
+
+import java.util.Scanner;
+
+interface IAdvancedArithmetic{
+	  int divisor_sum(int n);
+}
+
+class MyCalculator implements IAdvancedArithmetic{
+
+	@Override
+	 public int divisor_sum(int n) {
+			int sum = 0;
+			for (int i=1;i<n+1;i++)
+			{
+				if(n%i==0)
+				{
+					sum=sum+i;
+				}
+			}
+		return sum;
+		
+	}
+	
+}
+
+public class InterfacePolymorphismQues3 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int a= sc.nextInt();
+		if (a<1001)
+		{
+		MyCalculator mcalc=new MyCalculator();
+		System.out.println("Sum of all divisors of "+a+" is "+mcalc.divisor_sum(a));
+		}
+		else
+		{
+			System.out.println("Enter the Number not greater than 1000");
+		}
+
+	}
+
+}
