4e09d3b7c9fb37183bfdf7a536e1b22764f8039b
Sumit <kumar07sumit@gmail.com>
2020-08-31 10:18:54 +0530

    Assignments

--- Xebia_FIL_Assignments/src/day8exercise/VolOfAllBoxesQues5.java
+++ Xebia_FIL_Assignments/src/day8exercise/VolOfAllBoxesQues5.java
@@ -1,8 +1,9 @@
 package day8exercise;
 
+import java.util.Scanner;
+
 class Boxes{
-	public static int volume(){
-		int [][] arr1={{2,3,2},{6,6,7},{1,2,1}};
+	public static int volume(int [][] arr1){
 		int sum=0;
 		for (int i = 0; i < 3; i++) { 
 			int mul=1;
@@ -11,14 +12,34 @@ class Boxes{
             }
             	sum=sum+mul;
 		}
-		System.out.println("Total Volume of all three Boxes: "+sum);
+		System.out.println("Total Volume of all these three Boxes is: "+sum);
 		return sum;
 		
 	}
 }
 public class VolOfAllBoxesQues5 {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		Boxes.volume();
+		int[][] arr1 = new int[3][3];
+		 Scanner s1 = new Scanner(System.in);
+		 System.out.println("Enter the length, breadth and height of all three boxes, Press Enter after each value:");
+		 for(int i = 0 ; i < 3 ; i++){
+		 for(int j = 0 ; j < 3; j++)
+		 {
+		 arr1[i][j] = s1.nextInt();
+		 }
+		 System.out.println();
+		 }
+		 System.out.println("Three boxes Arrays are now represented as:");
+		 for(int i = 0 ; i < 3 ; i++){
+		 for(int j = 0 ; j < 3; j++)
+		 {
+		 System.out.print(arr1[i][j] + " " );
+		 }
+		 System.out.println();
+		 }
+		 System.out.println();
+		Boxes.volume(arr1);
 
 	}
 
